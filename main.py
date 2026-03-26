99aa38f550b0bc238cd6a47bd81a4c314d0312cb
milliafsd <milliafsd1@gmail.com>
2026-03-27 04:55:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import base64
 import os
 
 # -------------------- 1. ڈیٹا بیس سیٹ اپ --------------------
-DB_NAME = 'jamia_erp_v3.db'
+DB_NAME = 'jamia_millia_v1 (1) (1).db'
 
 def get_db_connection():
     return sqlite3.connect(DB_NAME, check_same_thread=False)
